41819dd08d554a9698f281419518944b6e66a2a3
rutuja patil <rutujapatil991@gmail.com>
2022-02-15 15:10:08 +0530

    commit message

---
+++ EmlpoyeeWageComputation/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmlpoyeeWageComputation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" =========Welcome To Employee Computation===");
+        }
+    }
+}
